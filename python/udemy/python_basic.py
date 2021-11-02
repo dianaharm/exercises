c88c02018fbe776096a02757d2add876af60e925
Diana Rodriguez <diana.rodriguez@team.neustar>
2021-11-01 22:06:21 -0600

    Adding examples of strings manipulation

--- python/udemy/python_basic.py
+++ python/udemy/python_basic.py
@@ -1,5 +1,6 @@
 
 import datetime
+import re
 
 def inputExample():
     '''
@@ -42,6 +43,45 @@ def variableType():
     # Using f-strings formats
     print(f"Variable called {myAge} is a type of {str(varType)}")
 
+    #Math operations with that input
+    print(f"Let's calculate how old you will be in 2050")
+    yearsToSum = 2050 - datetime.datetime.now().year
+    yearsToHave = yearsToSum + myAge
+    print(f"We are {yearsToSum} years from 2050, so you will have {str(yearsToHave)}") 
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def stringManipulation():
+    """
+    Example 3 - 
+        A litle bit of examples where we use some of strings manipulations
+    """
+    stringEx = "IAmLearningHowToProgramingPythonLanguage"
+    print(f"The lenght of the string {stringEx} is {str(len(stringEx))} characters")
+
+    print(f"All letters to upper: {stringEx.upper()}")
+
+    print(f"All letters to lower: {stringEx.lower()}")
+
+    spaces = " ".join(stringEx)
+    print(f"Add spaces between letters: {spaces}")
+
+    # Adding a regular expression, looking for the upper letter
+    # to insert a white space
+    spacesUpper = re.sub('([A-Z])', r' \1', stringEx)
+    print(f"Adding spaces after each upper letter: {spacesUpper}")
+
+    # Find if substring is in the original string
+    subStr = "PythonLanguage"
+    if subStr in stringEx:
+        print(f"Substring {subStr} is in the string {stringEx}")
+    
+    subStr = "The dog is black"
+    if subStr in stringEx:
+        print(f"Substring {subStr} is in the string {stringEx}")
+    else:
+        print(f"Substring {subStr} is NOT in the string {stringEx}")
+    
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Main method in order to call only a specific function, not the whole script
@@ -51,7 +91,10 @@ def main():
     # inputExample()
 
     # Example 2 - 
-    variableType()
+    #variableType()
+
+    # Example 3 -
+    stringManipulation()
 
 
 if __name__ == '__main__':
